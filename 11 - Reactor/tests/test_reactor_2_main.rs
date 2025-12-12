c223f01c41c4d19ca3da10959c679842c2938271
Peter-Frank Spierenburg <Peter.Spierenburg@geospectrum.ca>
2025-12-12 16:38:52 -0400

    Added 11 - Reactor

---
+++ 11 - Reactor/tests/test_reactor_2_main.rs
@@ -0,0 +1,16 @@
+const EXECUTABLE_UNDER_TEST: &str = toolbox::binary_path!("reactor_2");
+
+const TEST_CASES: &[(&str, &str)] = &[("", "Hello, world!\n")];
+
+#[test]
+fn test_that_the_executable_exists() {
+    assert!(std::fs::exists(EXECUTABLE_UNDER_TEST).unwrap());
+}
+
+#[test]
+fn test_that_the_executable_processes_the_input_and_produces_the_output() {
+    for &(input, expected_result) in TEST_CASES {
+        let child_output = toolbox::invoke_executable(EXECUTABLE_UNDER_TEST, input);
+        assert_eq!(child_output, expected_result);
+    }
+}
